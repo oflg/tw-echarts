a892d1b54a15e80d63297a99ecfdd9a44e4dcf55
Gk0Wk <Gk0Wk@users.noreply.github.com>
2021-12-24 14:06:09 +0000

    deploy: 646d937bcfb41c99181d2e412778ec6575d0ade8

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.4/workbox-sw.js');
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉Service Worker is working!`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬Service Worker won't work properly...`);
+}
+
+const { registerRoute } = workbox.routing;
+const { CacheFirst, StaleWhileRevalidate } = workbox.strategies;
+const { ExpirationPlugin } = workbox.expiration;
+const { precacheAndRoute, matchPrecache } = workbox.precaching;
+
+precacheAndRoute([{"revision":"2202d1024158299c249bb2eeb50dc494","url":"favicon.ico"},{"revision":"1ee08a80e6fd2231b86c661a7f4d514a","url":"index.html"},{"revision":"88bc0a8bc68dfdc8f7d716b2e8e3962d","url":"tiddlywikicore-5.2.1.js"}]);
+
+registerRoute(
+  /\.css$/,
+  // Use cache but update in the background.
+  new StaleWhileRevalidate({
+    // Use a custom cache name.
+    cacheName: 'css-cache',
+  })
+);
+
+registerRoute(
+  /\.(?:png|jpg|jpeg|svg|gif|woff2?|ttf)$/,
+  // Use the cache if it's available.
+  new CacheFirst({
+    cacheName: 'image-cache',
+    plugins: [
+      new ExpirationPlugin({
+        // Cache only a few images.
+        maxEntries: 100,
+        // Cache for a maximum of a week.
+        maxAgeSeconds: 7 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
+
+registerRoute(/\.js$/, new StaleWhileRevalidate());
+registerRoute(/(^\/$|index.html)/, new StaleWhileRevalidate());
